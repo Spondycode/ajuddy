0afc965f0ad70d968d4441405fe3485644d99ca7
David Allen <spondicious@me.com>
2023-10-09 22:24:30 +0200

    more work done

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 def home_view(request):
-    title = 'Welcome To The Pleasure Dome'
-    context = {'title' :title}
+    posts = Post.objects.all()
+    context = {'posts' :posts}
     return render(request, 'a_posts/home.html', context)
+
+
+def post_create_view(request):
+    return render(request, 'a_posts/post_create.html')
